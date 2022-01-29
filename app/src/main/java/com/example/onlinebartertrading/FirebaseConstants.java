3da2d9c3550875b6475a5e90ae4555e268fbe6aa
Arjun Banga <ar424886@dal.ca>
2022-01-30 04:22:28 +0530

    Updating firebase dependencies and adding Activity for registration.

---
+++ app/src/main/java/com/example/onlinebartertrading/FirebaseConstants.java
@@ -0,0 +1,11 @@
+package com.example.onlinebartertrading;
+
+public class FirebaseConstants {
+
+    private FirebaseConstants(){
+        // Required private constructor
+    }
+
+    public static final String FIREBASE_URL = "https://online-barter-trading-bc337-default-rtdb.firebaseio.com/";
+
+}
