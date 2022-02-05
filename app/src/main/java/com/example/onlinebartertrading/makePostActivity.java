e4a609599be1d67fd463068e90bac6243884dd4d
TGCanada <jacobhare153@hotmail.com>
2022-02-05 15:37:56 -0400

    Added failing tests for titleText

--- app/src/main/java/com/example/onlinebartertrading/makePostActivity.java
+++ app/src/main/java/com/example/onlinebartertrading/makePostActivity.java
@@ -3,12 +3,36 @@ package com.example.onlinebartertrading;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class makePostActivity extends AppCompatActivity {
+public class makePostActivity extends AppCompatActivity implements View.OnClickListener {
+    public static int maxTitleLength = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_post);
+        Button postButton = findViewById(R.id.makePostButton);
+        postButton.setOnClickListener(this);
+    }
+
+    protected String getTitleDesc(){
+
+        return null;
+    }
+
+    protected Boolean validTitleDesc(String title){
+
+        return false;
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/test/java/com/example/onlinebartertrading/ExampleUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrading/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.onlinebartertrading;
 
+import static com.example.onlinebartertrading.makePostActivity.maxTitleLength;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,29 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    static makePostActivity makePostActivity;
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void checkIfTitleIsEmpty(){
+        assertFalse(makePostActivity.validTitleDesc(""));
+    }
+
+    @Test
+    public void checkIfTitlePostIsToLong(){
+        String testString = "";
+        for (int i=0; i<maxTitleLength+1;i++){
+            testString+="1";
+        }
+        assertFalse(makePostActivity.validTitleDesc(testString));
+    }
+
+    @Test
+    public void checkIfValidTitlePost(){
+        assertTrue(makePostActivity.validTitleDesc("This is a valid title"));
     }
 }
