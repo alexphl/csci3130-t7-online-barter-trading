ba9d3f84e16e9f46498a9b6f0a5b8cdc5658918a
cipaianu <ph469663@dal.ca>
2022-02-06 18:25:20 -0400

    Revert "Added login espresso test to determine if login is successful and has transitioned to the home page"
    This reverts commit cb010c69871d27409bfa0a2087a5ff3b033fc264.

--- app/src/androidTest/java/com/example/onlinebartertrading/LoginEspressoTest.java
+++ app/src/androidTest/java/com/example/onlinebartertrading/LoginEspressoTest.java
@@ -4,22 +4,17 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static android.content.Intent.*;
 import static org.junit.Assert.*;
 
 import android.content.Context;
 
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -100,13 +95,5 @@ public class LoginEspressoTest {
         onView(withId(R.id.passwordField).perform(typeText("test")));
         onView(withId(R.id.loginBttn)).perform(click());
         onView(withId(R.id.loginStatus)).check(matches(withText(R.string.INVALID_PASSWORD)));
-    }
-
-    @Test
-    public void checkIfMoved2HomePage() {
-        onView(withId(R.id.emailField).perform(typeText("testemail@gmail.com")));
-        onView(withId(R.id.passwordField).perform(typeText("testingPassword")));
-        onView(withId(R.id.loginBttn)).perform(click());
-        intended(hasComponent(makePostActivity.class.getName()));
     }
 }
