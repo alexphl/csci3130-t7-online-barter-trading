ca2ad908ab65c07cb5c50b5e445fca68087d5fbd
cipaianu <ph469663@dal.ca>
2022-02-06 14:22:45 -0400

    Added Espresso Test to check if the login page is visible

--- app/src/androidTest/java/com/example/onlinebartertrading/LoginEspressoTest.java
+++ app/src/androidTest/java/com/example/onlinebartertrading/LoginEspressoTest.java
@@ -1,13 +1,20 @@
 package com.example.onlinebartertrading;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
 import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Rule;
-import org.junit.runner.RunWith;
-import androidx.test.espresso.*;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import org.junit.Test;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -29,4 +36,17 @@ public class LoginEspressoTest {
     public static void tearDown() {
         System.gc();
     }
+
+    @Test
+    public void useAppContext() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.onlinebartertrading", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkIfLoginPageIsVisible() {
+        onView(withId(R.id.emailField).check(matches(withText(R.string.VALID_EMAIL))));
+        onView(withId(R.id.passwordField).check(matches(withText(R.string.VALID_PASSWORD))));
+    }
+
 }
