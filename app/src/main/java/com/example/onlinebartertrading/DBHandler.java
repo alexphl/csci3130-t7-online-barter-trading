e56187bcee4fd1f77e1923e143383d4987e78d6f
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-02-07 15:59:33 -0400

    Refactor DBHandler, add getter stubs

--- app/src/main/java/com/example/onlinebartertrading/DBHandler.java
+++ app/src/main/java/com/example/onlinebartertrading/DBHandler.java
@@ -1,7 +1,12 @@
 package com.example.onlinebartertrading;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -11,6 +16,14 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class DBHandler {
+
+    private DatabaseReference dbRef;
+
+    protected void initializeDatabase() {
+        dbRef = FirebaseDatabase
+                .getInstance(FirebaseConstants.FIREBASE_URL)
+                .getReference();
+    }
 
     public String hashString(String password) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-512");
@@ -22,12 +35,20 @@ public class DBHandler {
 
     private void addUserData() {
         String uuid = UUID.randomUUID().toString();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance(FirebaseConstants.FIREBASE_URL).getReference().child(FirebaseConstants.USERS_COLLECTION).child(uuid);
+        DatabaseReference userRef = dbRef.child(FirebaseConstants.USERS_COLLECTION).child(uuid);
         HashMap<String, String> userData = new HashMap<>();
         userData.put("first_name", "FIRSTNAME_HERE");
         userData.put("last_name", "LASTNAME_HERE");
         userData.put("email", "EMAIL_HERE");
-        dbRef.push().setValue(userData);
+        userRef.push().setValue(userData);
+    }
+
+    public String getUserEmail(String uuid) {
+        return null;
+    }
+
+    public String getUserName(String uuid) {
+        return null;
     }
 
 }
