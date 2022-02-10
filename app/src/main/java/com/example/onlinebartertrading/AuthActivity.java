f53de4c13e694c2082a48b0890e572a392109e9c
Arjun Banga <ar424886@dal.ca>
2022-02-10 12:24:18 +0530

    Implemented pager with tab layout for auth activity.

--- app/src/main/java/com/example/onlinebartertrading/AuthActivity.java
+++ app/src/main/java/com/example/onlinebartertrading/AuthActivity.java
@@ -1,25 +1,70 @@
 package com.example.onlinebartertrading;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.telephony.SignalStrength;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AuthActivity extends AppCompatActivity {
 
     TabLayout panes;
-    ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        panes = (TabLayout) findViewById(R.id.authTabs);
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        FragmentStateAdapter pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        new TabLayoutMediator(panes, viewPager, (tab, position) -> {
+            if(position == 0) {
+                tab.setText("Sign Up");
+            }
+            else if(position == 1) {
+                tab.setText("Log In");
+            }
+        }).attach();
     }
+
 
     protected boolean isValidEmailAddress(String emailAddress) {
         return emailAddress != null && emailAddress.matches(".*@.*[\\.].*$");
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            if(position == 0) {
+                return new SignupFormFragment();
+            }
+            else return new LoginFormFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
+        }
     }
 
 
