1e6f6ec8573b0498536343901b9052b0da6c9d26
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-02-13 15:01:38 -0400

    Revert "Merge branch 'dev-posts' into 'develop'"
    This reverts commit 403529acc94f4b61ea21a56e79648ba0bb545ccc, reversing
    changes made to c2f9e641a8829b24c0c95f4282a0186e6993995f.

--- app/src/main/java/com/example/onlinebartertrading/FirebaseConstants.java
+++ app/src/main/java/com/example/onlinebartertrading/FirebaseConstants.java
@@ -7,6 +7,5 @@ public class FirebaseConstants {
     }
 
     public static final String FIREBASE_URL = "https://online-barter-trading-bc337-default-rtdb.firebaseio.com/";
-    public static final String USERS_COLLECTION = "users";
 
 }
--- app/src/test/java/com/example/onlinebartertrading/ExampleUnitTest.java
+++ app/src/test/java/com/example/onlinebartertrading/ExampleUnitTest.java
@@ -1,9 +1,5 @@
 package com.example.onlinebartertrading;
 
-import static com.example.onlinebartertrading.MakePostActivity.maxTitleLength;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,12 +10,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-
-
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-
 }
