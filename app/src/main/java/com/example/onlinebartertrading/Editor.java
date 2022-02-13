18a1d77f706acee3674a04bd2022c2173a805949
Shun Li <sh615496@dal.ca>
2022-02-13 16:02:39 -0400

    Update Editor.java

--- app/src/main/java/com/example/onlinebartertrading/Editor.java
+++ app/src/main/java/com/example/onlinebartertrading/Editor.java
@@ -29,7 +29,7 @@ public class Editor extends ArrayAdapter<String> {
     }
 
 
-//show item's view
+//getView() is a method that run several times, every time your program inflate a row in your list it will be running. The parent is your custom adapter that you inflate a row into it. convertView is the GUI(view) of the row in the Position position in your adapter.
     @Override
     public View getView(int position,  View convertView, ViewGroup parent) {
         View view = convertView;
