706fa4f4165d72b20a69252a134257d4a45cd776
cipaianu <ph469663@dal.ca>
2022-02-13 13:57:41 -0400

    Added Espresso Test to check if fragment slides properly

--- app/src/androidTest/java/com/example/onlinebartertrading/SignupEspressoTests.java
+++ app/src/androidTest/java/com/example/onlinebartertrading/SignupEspressoTests.java
@@ -107,6 +107,11 @@ public class SignupEspressoTests {
         onView(withId(R.id.statusLabel)).check(matches(withText(R.string.diffPassword)));
     }
 
-
-
+    /*** User Acceptance Test - V**/
+    @Test
+    public void checkIfFragSlide() {
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.emailField)).check(matches(withText("")));
+        onView(withId(R.id.passwordField)).check(matches(withText("")));
+    }
 }
