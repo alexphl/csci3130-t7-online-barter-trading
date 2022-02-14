c6c23b6f24eafdd9a206300475574dc31160cc56
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-02-13 20:30:06 -0400

    Refactor Editor

--- app/src/main/java/com/example/onlinebartertrading/Editor.java
+++ app/src/main/java/com/example/onlinebartertrading/Editor.java
@@ -12,42 +12,41 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
+/**
+ * Post editing interface
+ **/
 public class Editor extends ArrayAdapter<String> {
     Context context;
     ArrayList<String> name = new ArrayList<>();
     ArrayList<String> detail = new ArrayList<>();
     ArrayList<String> value = new ArrayList<>();
 
-    
+    /**
+     * @param context
+     * @param name      User provided name
+     * @param detail    User provided details
+     * @param value     User provided item valuation
+     */
     public Editor(ShowDetails context, ArrayList<String> name, ArrayList<String> detail, ArrayList<String> value) {
         super(context, R.layout.activity_listview, R.id.textview2, name);
         this.context = context;
         this.name = name;
         this.detail = detail;
         this.value = value;
-
     }
 
-/**
-*getView
-*The parent is your custom adapter that you inflate a row into it. convertView is the GUI(view) of the row in the Position position in your adapter.
-**/
-
+    /**
+     * The parent is your custom adapter that you inflate a row into it.
+     * convertView is the GUI(view) of the row in the Position position in your adapter.
+     **/
     @Override
     public View getView(int position,  View convertView, ViewGroup parent) {
         View view = convertView;
         NameSet nameSet;
-        if(view != null)
-        {
+        if(view != null) {
             nameSet = (NameSet) view.getTag();
-        }else
-        {
-
-/**
-*The LayoutInflater takes layout XML-files and creates different View-objects from its contents.
-**/
-
-            
+        } else {
+            //The LayoutInflater takes layout XML-files and creates different View-objects from its contents.
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = layoutInflater.inflate(R.layout.activity_listview, parent, false);
             nameSet = new NameSet(view);
