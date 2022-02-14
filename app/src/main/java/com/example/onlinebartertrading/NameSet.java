07d242efeaa2c81c44a6c3a6c52571f25790d01a
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-02-13 20:24:45 -0400

    Refactor NameSet

--- app/src/main/java/com/example/onlinebartertrading/NameSet.java
+++ app/src/main/java/com/example/onlinebartertrading/NameSet.java
@@ -3,17 +3,16 @@ package com.example.onlinebartertrading;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * NameSet, used exclusively in Editor class
+ */
 public class NameSet {
-    TextView name;
-    TextView detail;
-    TextView value;
+    TextView name, detail, value;
 
-    NameSet(View view)
-    {
+    NameSet(View view) {
         name = view.findViewById(R.id.textView1);
         value = view.findViewById(R.id.textView3);
         detail = view.findViewById(R.id.textview2);
-
     }
 
 
