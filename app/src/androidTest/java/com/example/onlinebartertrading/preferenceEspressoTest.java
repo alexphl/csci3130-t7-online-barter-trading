96d41a257fce6842173d7af9cec50fadd22ab17e
TGCanada <jacobhare153@hotmail.com>
2022-02-21 22:36:37 -0400

    Added test for basic activity layout

---
+++ app/src/androidTest/java/com/example/onlinebartertrading/preferenceEspressoTest.java
@@ -0,0 +1,42 @@
+package com.example.onlinebartertrading;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class preferenceEspressoTest {
+
+
+    @Rule
+    public ActivityScenarioRule<preferenceActivity> myRule = new ActivityScenarioRule<>(preferenceActivity.class);
+
+    @BeforeClass
+    public static void setup() {
+        Intents.init();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
+    @Test
+    public void checkIfPreferenceIsVisible() {
+        onView(withId(R.id.carChip)).check(matches(isDisplayed()));
+    }
+}
+
