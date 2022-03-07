00eb2dc8a3caf65caf7b9e4adb537ad0a33e1cfa
Mateo <mt399613@dal.ca>
2022-03-07 18:25:58 -0400

    Implemented funcionality to check if a user had previous stored preferences

--- app/src/main/java/com/example/onlinebartertrading/DBUserPreference.java
+++ app/src/main/java/com/example/onlinebartertrading/DBUserPreference.java
@@ -1,19 +1,48 @@
 package com.example.onlinebartertrading;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.UUID;
 
 public class DBUserPreference {
 
-    private final DatabaseReference dbRef;
     private final DatabaseReference userRef;
+    private boolean hasPreferences;
 
     public DBUserPreference(String email) {
-        dbRef = FirebaseDatabase.getInstance(FirebaseConstants.FIREBASE_URL).getReference();
+        DatabaseReference dbRef = FirebaseDatabase
+                .getInstance(FirebaseConstants.FIREBASE_URL)
+                .getReference();
 
         String uuid = UUID.nameUUIDFromBytes(email.getBytes()).toString();
         userRef = dbRef.child(FirebaseConstants.USERS_COLLECTION).child(uuid);
+        hasPreferences = false;
+        setHasPreferences();
+    }
+
+    private void setHasPreferences() {
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild("preferences")) {
+                    hasPreferences = true;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("DATABASE ERROR: " + error.getMessage());
+            }
+        });
+    }
+
+    public boolean hasPreferences() {
+        return hasPreferences;
     }
 }
