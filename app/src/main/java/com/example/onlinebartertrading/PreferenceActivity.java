9f50dea7e55e4579880766aac2ac4d294866852f
Mateo <mt399613@dal.ca>
2022-03-07 19:29:46 -0400

    Refactored PreferenceClass and added methods to activity

--- app/src/main/java/com/example/onlinebartertrading/PreferenceActivity.java
+++ app/src/main/java/com/example/onlinebartertrading/PreferenceActivity.java
@@ -1,7 +1,9 @@
 package com.example.onlinebartertrading;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +11,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.chip.ChipGroup;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
+import java.util.UUID;
 
 public class PreferenceActivity extends AppCompatActivity implements View.OnClickListener {
     //km
@@ -21,9 +29,47 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
 
+        setPreferences();
+
         Button enterButton = findViewById(R.id.preferenceButton);
         enterButton.setOnClickListener(this);
+    }
 
+    // Checks if user has saved preferences and sets them if true
+    protected void setPreferences() {
+
+        DatabaseReference userRef = initializeUserDBRef();
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild("preferences")) {
+                    // Set preferences on edittexts and chips
+                }
+                else {
+                    // no preferences exist so load defaults
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("DATABASE ERROR: " + error.getMessage());
+            }
+        });
+    }
+
+    protected DatabaseReference initializeUserDBRef() {
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+
+        DatabaseReference dbRef = FirebaseDatabase
+                .getInstance(FirebaseConstants.FIREBASE_URL)
+                .getReference();
+
+        String uuid = UUID.nameUUIDFromBytes(email.getBytes()).toString();
+        DatabaseReference userRef = dbRef.child(FirebaseConstants.USERS_COLLECTION).child(uuid);
+
+        return userRef;
     }
 
     protected void setStatusMessage(String message){
@@ -131,11 +177,9 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
         setStatusMessage(errorMessage);
 
         if (errorMessage.equals("")){
-            PreferenceClass userPref = new PreferenceClass();
-            userPref.setDistance(maxDistance);
-            userPref.setMaxValue(maxValue);
-            userPref.setMinValue(minValue);
-            userPref.setTags(selectedTags);
+            PreferenceClass userPref =
+                    new PreferenceClass(selectedTags, minValue, maxValue, maxDistance);
+
 
             //switch to new activity
         }
--- app/src/main/java/com/example/onlinebartertrading/PreferenceClass.java
+++ app/src/main/java/com/example/onlinebartertrading/PreferenceClass.java
@@ -4,46 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PreferenceClass {
+
     private List<Integer> tags;
     private int minValue;
     private int maxValue;
     private int distance;
-    public PreferenceClass(){
-        tags = new ArrayList<>();
-        minValue = 0;
-        maxValue = MakePostActivity.maxValue;
-        distance = PreferenceActivity.MAX_DISTANCE;
+
+    public PreferenceClass(List<Integer> tags, int minValue, int maxValue, int distance){
+        this.tags = tags;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.distance = distance;
     }
 
     public List<Integer> getTags() {
         return tags;
     }
 
-    public int getDistance() {
-        return distance;
+    public int getMinValue() {
+        return minValue;
     }
 
     public int getMaxValue() {
         return maxValue;
     }
 
-    public int getMinValue() {
-        return minValue;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-
-    public void setMaxValue(int maxValue) {
-        this.maxValue = maxValue;
-    }
-
-    public void setMinValue(int minValue) {
-        this.minValue = minValue;
-    }
-
-    public void setTags(List<Integer> tags) {
-        this.tags = tags;
+    public int getDistance() {
+        return distance;
     }
 }
