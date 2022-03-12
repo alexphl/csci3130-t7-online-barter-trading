ef551b911f9e5c397588bc3ac2f8db60dc538e2d
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-03-12 19:39:26 -0400

    User: implement setters and getters

--- app/src/main/java/com/example/onlinebartertrading/User.java
+++ app/src/main/java/com/example/onlinebartertrading/User.java
@@ -13,15 +13,40 @@ public class User implements Serializable {
     private LatLng lastLocation;
     private Preferences preferences;
 
-    public User(String email, LatLng lastLocation) {
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LatLng getLastLocation() {
+        return lastLocation;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
+    }
+
+    public User(String email) {
         this.email = email;
+    }
+
+    public void setLastLocation(LatLng lastLocation) {
         this.lastLocation = lastLocation;
     }
 
-    public User(String email, LatLng lastLocation, Preferences preferences) {
-
+    public void setPreferences(Preferences preferences) {
+        this.preferences = preferences;
     }
-
-    public void setPreferences(Preferences preferences) {this.preferences = preferences;}
 
 }
