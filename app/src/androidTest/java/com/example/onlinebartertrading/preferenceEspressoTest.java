99d7c306d1ce43c0311e6251eba361fc8c8637a3
Arjun Banga <ar424886@dal.ca>
2022-03-12 20:04:45 -0400

    Fixing makePost and preferenceEspresso tests

--- app/src/androidTest/java/com/example/onlinebartertrading/preferenceEspressoTest.java
+++ app/src/androidTest/java/com/example/onlinebartertrading/preferenceEspressoTest.java
@@ -11,6 +11,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -25,9 +29,14 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class preferenceEspressoTest {
 
-
+    static Intent intent = new Intent(ApplicationProvider.getApplicationContext(), PreferenceActivity.class);
+    static Bundle bundle = new Bundle();
+    static {
+        bundle.putString("userEmail", "testuser1@email.com");
+        intent.putExtras(bundle);
+    }
     @Rule
-    public ActivityScenarioRule<PreferenceActivity> myRule = new ActivityScenarioRule<>(PreferenceActivity.class);
+    public ActivityScenarioRule<PreferenceActivity> activityScenarioRule = new ActivityScenarioRule<>(intent);
 
 
 
