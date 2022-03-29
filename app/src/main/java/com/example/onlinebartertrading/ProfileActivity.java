301e88699028a39e5795f17529c2faa85b03de7f
Mateo <mt399613@dal.ca>
2022-03-29 17:35:50 -0300

    Implemented general structure to load history

--- app/src/main/java/com/example/onlinebartertrading/ProfileActivity.java
+++ app/src/main/java/com/example/onlinebartertrading/ProfileActivity.java
@@ -1,14 +1,68 @@
 package com.example.onlinebartertrading;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.onlinebartertrading.configs.FirebaseConstants;
+import com.example.onlinebartertrading.entities.Preferences;
+import com.example.onlinebartertrading.entities.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    User user;
+    DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        user = (User) getIntent().getSerializableExtra("user");
+        loadHistory();
+    }
+
+    protected void initializeUserDBRef() {
+        DatabaseReference dbRef = FirebaseDatabase
+                .getInstance(FirebaseConstants.FIREBASE_URL)
+                .getReference();
+
+        String uuid = UUID.nameUUIDFromBytes(user.getEmail().getBytes()).toString();
+        userRef = dbRef.child(FirebaseConstants.USERS_COLLECTION).child(uuid);
+    }
+
+    /**
+     * Sets the preferences of a user if they are stored in database
+     */
+    protected void loadHistory() {
+
+        initializeUserDBRef();
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String message;
+                if (snapshot.hasChild("history_provider")) {
+                    // Load history
+                }
+                else {
+                    // No history to load
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("DATABASE ERROR: " + error.getMessage());
+            }
+        });
     }
 }
